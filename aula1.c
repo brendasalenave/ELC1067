b6d347bce87055fb57313117532adc8ec90be863
Guest <brendasalenave@live.com>
2015-03-05 09:43:21 -0300

    nova alteracao

--- aula1.c
+++ aula1.c
@@ -1,4 +1,24 @@
-#incude <stdio.h>
+#include <stdio.h>
 
-int main (){
+/*
+- Lê: 1. Matricula (número)
+      2. Nome: (string)
+- Escreve: saida.txt
+*/
+
+void main (){
+    int matricula;
+    char nome[100];
+    FILE *f;
+
+    printf("Matricula: ");
+    scanf("%d", &matricula);
+    printf("Nome: ");
+    scanf("%s", nome);
+
+    f = fopen("saida.txt", "w");
+    fprintf(f,"%d %s \n", matricula, nome);
+    //fprintf(stdout, "ola \n");
+    fclose(f);
+
 }
