1ac3162b15eb69a901bfd075846e5167908636f4
brendasalenave <brendasalenave@live.com>
2015-03-10 11:31:22 -0300

    T1
    trabalho1

---
+++ T1/t1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void leAlunos(int* matriculas, char** nomes, int* n){
+
+}
+
+int main(int argc, char** argv){
+    int matricula[50];
+    char nomes[50][50];
+    int n;
+
+    leAlunos(matriculas, nomes, &n);
+
+}
+
