ba3431056c5c3b6754ca17241f847eb482f7591a
brendasalenave <brendasalenave@live.com>
2015-03-10 11:57:12 -0300

    T1
    nova alteração

--- T1/t1.c
+++ T1/t1.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void leAlunos(int* matriculas, char** nomes, int* n){
+    FILE *f = fopen("alunos.txt","r");
+    int mat, i;
+    int linha = 0;
+    char c;
+    char nome[50];
 
+    while(FEOF(f) != 0){
+        fscanf(f,"%d", &mat);
+        c = fgetc(f);
+
+        while(c == ' '){
+            c = fgetc(f);
+        }
+
+        while(c != '\n'){
+            nome[i] = c;
+            c = fgetc(f);
+            i++;
+        }
+        nome[i] = '\0';
+        matriculas[linha] = mat;
+        strcpy(nomes[linha], nome);
+        linha++;
+    }
+    n = linha;
+    fclose(f);
 }
 
 int main(int argc, char** argv){
@@ -9,7 +35,7 @@ int main(int argc, char** argv){
     char nomes[50][50];
     int n;
 
-    leAlunos(matriculas, nomes, &n);
+    leAlunos(matricula, nomes, &n);
 
 }
 
