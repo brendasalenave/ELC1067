78bc205b04dfba1028acbb51077f6a9f63add30e
brendasalenave <brendasalenave@live.com>
2015-03-31 19:07:03 -0300

    T3
    não sei

--- T3/vetor.c
+++ T3/vetor.c
@@ -1,91 +1,96 @@
 /*
- * vetor.c
- * TAD que implementa um vetor dinamico.
- */
-
+* vetor.c
+* TAD que implementa um vetor dinamico.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
 #include "vetor.h"
 #include "carta.h"
 #include "memo.h"
-
 struct vetor {
-	carta* baralho;   /* baralho - vetor de cartas */
-	int n;		/* número de cartas */
+    carta* baralho; /* baralho - vetor de cartas */
+    int max;
+    int n; /* número de cartas */
 }; //vetor_t
 
 vetor_t* vetor_cria(void){
     int i;
-	struct vetor *vet = (struct vetor*)memo_aloca(sizeof(struct vetor));
-	vet->n = 0;
-	vet->baralho = (carta*)memo_aloca(50*sizeof(carta));
-	for(i=0; i<50; i++){
+    int max = 50;
+    struct vetor *vet = (struct vetor*)memo_aloca(sizeof(struct vetor));
+    vet->n = 0;
+    vet->baralho = (carta*)memo_aloca(max*sizeof(carta));
+    for(i=0; i<50; i++){
         vet->baralho[i] = NULL; //inicializa baralho com NULL
-	}
-	return vet;
+    }
+    return vet;
 }
 
 void vetor_destroi(vetor_t* vet){ //Ok
-	memo_libera(vet->baralho);
-	memo_libera(vet);
+    memo_libera(vet->baralho);
+    memo_libera(vet);
 }
 
 /* retorna o número de elementos no vetor vet. */
 int vetor_numelem(vetor_t *vet){ // Ok
     int i=0;
-	while(vet->baralho[i] != NULL){
+    while(vet->baralho[i] != NULL){
         vet->n++;
         i++;
-	}
-	return vet->n;
+    }
+    return vet->n;
 }
 
 /* insere uma carta no vetor vet, na posição indice.
- * se a carta já existe nessa posição, as cartas subsequentes avançam uma
- * posição.
- * se o vetor não tem mais espaço (cheio), utiliza realloc.
- */
+* se a carta já existe nessa posição, as cartas subsequentes avançam uma
+* posição.
+* se o vetor não tem mais espaço (cheio), utiliza realloc.
+*/
 void vetor_insere_carta(vetor_t *vet, int indice, carta c){
+    int i;
+    int ult = vet->n - 1;
+    int tam = vet->max + 10;
+
     if(vet->baralho[indice] == NULL)
         vet->baralho[indice] = c;
-    else
+    else{ // verifica se vetor cheio
+        if(vet->baralho[ult-1] != NULL){ // vetor cheio
+            vet->baralho = (carta*)realloc(vet->baralho, tam*sizeof(carta));
+            for(i=ult; i>=indice; i--){
+                vet->baralho[i] = vet->baralho[i+1];
+            }
+            vet->baralho[indice] = c;
+        //ponteiro =(int *) realloc( ponteiro, n*sizeof(int) );
+        }
     }
-
-	/* TODO aqui */
-	vet->n++;
+    /* TODO aqui */
+    vet->n++;
 }
-
 /* remove uma carta que estava na posição indice.
- * As cartas depois de indice recuam 1 posição à frente.
- * Retorna a carta original que estava na posição indice do vetor vet.
- */
+* As cartas depois de indice recuam 1 posição à frente.
+* Retorna a carta original que estava na posição indice do vetor vet.
+*/
 carta vetor_remove_carta(vetor_t *vet, int indice){
     int i;
-
-	carta* c = (struct carta*)memo_aloca(sizeof(carta));
+    int ultimo = vet->n;
+    carta c;
     c = vet->baralho[indice];
-	for(i=vet->n; i > indice; i--){
-        vet->baralho[i] = vet->baralho[i-1];
-	}
-
-	return c;
-
-	//vet->n--;
-	//return NULL;
+    for(i=indice; i < vet->n-1; i++){
+        vet->baralho[i] = vet->baralho[i+1];
+    }
+    vet->n--;
+    vet->baralho[ultimo-1] = NULL;
+    return c;
 }
-
 /* retorna o dado na posição indice do vetor vet */
-carta vetor_acessa_carta(vetor_t *vet, int indice){
+carta vetor_acessa_carta(vetor_t *vet, int indice){ //OK
     if(vet->baralho[indice] != NULL)
         return vet->baralho[indice];
     else return NULL;
 }
-
 /* retorna true se o vetor vet for válido */
 bool vetor_valido(vetor_t *vet){
-	if(vet != NULL)
+    if(vet != NULL)
         return true;
     else
         return false;
