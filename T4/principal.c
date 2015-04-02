03ebe00ac3a68b9ff4d97fdbf04cd486e4c5d058
brendasalenave <brendasalenave@live.com>
2015-04-02 11:22:25 -0300

    T4
    up

--- T4/principal.c
+++ T4/principal.c
@@ -1,21 +1,21 @@
 /*
  * principal.c Esse programa testa as TADs implementadas para um jogo
  * solitário.
- * 
+ *
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014, 2015 João V. Lima, UFSM 2005       Benhur Stein, UFSM
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -31,9 +31,24 @@
 #include "memo.h"
 #include "jogo.h"
 
-int 
-main(int argc, char **argv)
-{
+void inicia_jogo(jogo solit){
+    vetor_t* cartas = vetor_cria();
+    vetor_t* fora_ordem = vetor_cria();
+
+    int i, j;
+
+    for(i=0; i<=3; i++){
+        for(j=1; j<=13; j++){
+            vetor_insere_carta(cartas, (i*13)+j-1, carta_cria(i,j));
+        }
+    }
+    for(i=0; i < vetor_numelem(cartas); i++){
+
+    }
+
+}
+
+int main(int argc, char **argv){
 	jogo		solit;
 	solit = jogo_cria();
 
---
+++ T4/vetor.c
@@ -0,0 +1,92 @@
+/*
+* vetor.c
+* TAD que implementa um vetor dinamico.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "vetor.h"
+#include "carta.h"
+#include "memo.h"
+struct vetor {
+    carta* baralho; /* baralho - vetor de cartas */
+    int max;
+    int n; /* número de cartas */
+}; //vetor_t
+
+vetor_t* vetor_cria(void){
+    int i;
+    int max = 50;
+    struct vetor *vet = (struct vetor*)memo_aloca(sizeof(struct vetor));
+    vet->n = 0;
+    vet->baralho = (carta*)memo_aloca(max*sizeof(carta));
+    for(i=0; i<50; i++){
+        vet->baralho[i] = NULL; //inicializa baralho com NULL
+    }
+    return vet;
+}
+
+void vetor_destroi(vetor_t* vet){ //Ok
+    memo_libera(vet->baralho);
+    memo_libera(vet);
+}
+
+/* retorna o número de elementos no vetor vet. */
+int vetor_numelem(vetor_t *vet){ // Ok
+    return vet->n;
+}
+
+/* insere uma carta no vetor vet, na posição indice.
+* se a carta já existe nessa posição, as cartas subsequentes avançam uma
+* posição.
+* se o vetor não tem mais espaço (cheio), utiliza realloc.
+*/
+void vetor_insere_carta(vetor_t *vet, int indice, carta c){
+    int i;
+    int ult = vet->n - 1;
+    int tam = vet->max + 10;
+
+    if(vet->baralho[indice] == NULL)
+        vet->baralho[indice] = c;
+    else{ // verifica se vetor cheio
+        if(vet->baralho[ult-1] != NULL){ // vetor cheio
+            vet->baralho = (carta*)realloc(vet->baralho, tam*sizeof(carta));
+            for(i=ult; i>=indice; i--){
+                vet->baralho[i+1] = vet->baralho[i];
+            }
+            vet->baralho[indice] = c;
+        //ponteiro =(int *) realloc( ponteiro, n*sizeof(int) );
+        }
+    }
+    /* TODO aqui */
+    vet->n++;
+}
+/* remove uma carta que estava na posição indice.
+* As cartas depois de indice recuam 1 posição à frente.
+* Retorna a carta original que estava na posição indice do vetor vet.
+*/
+carta vetor_remove_carta(vetor_t *vet, int indice){
+    int i;
+    int ultimo = vet->n;
+    carta c;
+    c = vet->baralho[indice];
+    for(i=indice; i < vet->n-1; i++){
+        vet->baralho[i] = vet->baralho[i+1];
+    }
+    vet->n--;
+    vet->baralho[ultimo-1] = NULL;
+    return c;
+}
+/* retorna o dado na posição indice do vetor vet */
+carta vetor_acessa_carta(vetor_t *vet, int indice){ //OK
+    if(vet->baralho[indice] != NULL)
+        return vet->baralho[indice];
+    else return NULL;
+}
+/* retorna true se o vetor vet for válido */
+bool vetor_valido(vetor_t *vet){
+    if(vet->baralho != NULL)
+        return true;
+    else
+        return false;
+}
