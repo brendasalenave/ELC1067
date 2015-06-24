364e47588b079fa72a50a2408f40ae1ac198e106
brendasalenave <brendasalenave@live.com>
2015-06-24 00:13:43 -0300

    aeeaea

--- T8/principal.c
+++ T8/principal.c
@@ -20,42 +20,49 @@ int main(){
 
         int nVertices = 0;
         int nArestas = 0;
-        int i = 0;
+        int i, k;
 
         while(feof(f) == 0){
             //fflush(stdin);
             if(fscanf(f,"%d %d", &nVertices, &nArestas) < 0)
                 break;
+            fflush(stdin);
 
             printf("\nNUMERO DE VERTICES: %d", nVertices);
-            printf("\nNUMERO DE ARESTAS: %d", nArestas);
+            printf("\nNUMERO DE ARESTAS: %d\n\n", nArestas);
             //break;
 
-            c = fgetc(f);
-            while(c != '\n'){  //avança até nova linha
+            for(k = 0; k < nVertices; k++){
+                i = 0;
                 c = fgetc(f);
+                while(c != '\n'){  //avança até nova linha
+                    c = fgetc(f);
+                    fflush(stdin);
+                }
+
+                do{
+                    c = fgetc(f);
+                    fflush(stdin);
+                    chave[i] = c;
+                    i++;
+                }while(c != ' ');
+                    chave[i] = '\0';
+
+                //while(c == ' '){ //avança até a proxima palavra
+                //    c = fgetc(f);
+                //}
+                i = 0;
+                while(c != '\n'){
+                    nome[i] = c;
+                    c = fgetc(f);
+                    fflush(stdin);
+                    i++;
+                }
+                nome[i] = '\0';
+
+                printf("\n%s %s\n", chave, nome);
             }
-
-            do{
-                c = fgetc(f);
-                chave[i] = c;
-                i++;
-            }while(c != ' ');
-
-            //while(c == ' '){ //avança até a proxima palavra
-            //    c = fgetc(f);
-            //}
-            i = 0;
-            fflush(stdin);
-            while(c != '\n'){
-            nome[i] = c;
-            c = fgetc(f);
-            i++;
-        }
-
-            printf("\n%s  %s\n", chave, nome);
             break;
-
         }
         grafo_destroi(g);
         memo_libera(nome);
